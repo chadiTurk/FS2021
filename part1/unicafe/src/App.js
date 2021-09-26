5747e624af82b27ca21623cff09fa05e5a68b5e0
Chadi <chadi_turk@dlsu.edu.ph>
2021-09-26 18:29:17 +0800

    unicafe step 3 & 4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -16,9 +16,24 @@ const Header = ({headerText}) =>{
 
 
 const Content = (props) =>{
+
+  const isAllZero = props.statValues.every(item => item === 0);
+
+  if(isAllZero)
+    return(
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+
   return(
     <div>
-      <p>{props.statisticsText} {props.statisticsVal}</p>
+      <p>{props.buttonText[0]} {props.statValues[0]}</p>
+      <p>{props.buttonText[1]} {props.statValues[1]}</p>
+      <p>{props.buttonText[2]} {props.statValues[2]}</p>
+      <p>{props.summaryText[0]} {props.numericalSummaries[0]}</p>
+      <p>{props.summaryText[1]} {props.numericalSummaries[1]}</p>
+      <p>{props.summaryText[2]} {props.numericalSummaries[2]}</p>
     </div>
   )
 
@@ -32,10 +47,16 @@ const App = () => {
   const [bad, setBad] = useState(0)
   const headerFeedbackText = "give feedback"
   const headerStatisticsText = "statistics"
+
   const buttonText = ["good","neutral","bad"]
   const summaryText = ["all","average","positive"]
+  const statValues = [good,neutral,bad]
 
   const all = bad+good+neutral
+  const average = ((good-bad)/all)*100
+  const positive = (good/all)*100
+
+  const numericalSummaries = [all,average,positive]
 
   const updateGood = () =>{
     setGood(good + 1)
@@ -56,12 +77,7 @@ const App = () => {
       <Button onClickEvent = {updateNeutral} buttonText = {buttonText[1]}/>
       <Button onClickEvent = {updateBad} buttonText = {buttonText[2]}/>
       <Header headerText = {headerStatisticsText}/>
-      <Content statisticsText = {buttonText[0]} statisticsVal = {good}/>
-      <Content statisticsText = {buttonText[1]} statisticsVal = {neutral}/>
-      <Content statisticsText = {buttonText[2]} statisticsVal = {bad}/>
-      <Content statisticsText = {summaryText[0]} statisticsVal = {all}/>
-      <Content statisticsText = {summaryText[1]} statisticsVal = {((good-bad)/all)*100}/>
-      <Content statisticsText = {summaryText[2]} statisticsVal = {(good/all)*100}/>
+      <Content buttonText = {buttonText} summaryText = {summaryText} numericalSummaries = {numericalSummaries} statValues = {statValues}/>
 
     </div>
   )
