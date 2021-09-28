8fc7a821b31d44d1ec84aa147bacaba1866354e5
Chadi <chadi_turk@dlsu.edu.ph>
2021-09-28 14:38:07 +0800

    phonebook step 3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -8,6 +8,7 @@ const App = () => {
   const [ persons, setPersons ] = useState([
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) =>{
     event.preventDefault()
@@ -19,17 +20,26 @@ const App = () => {
     }
       
     let newPerson = {
+      id:persons.length + 1,
       name:newName,
-      id:persons.length + 1
+      number:newNumber
+      
     }
 
     setPersons(persons.concat(newPerson))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) =>{
     setNewName(event.target.value)
   }
+
+  const handleNumberChange = (event) =>{
+    setNewNumber(event.target.value)
+  }
+    
+  
 
   return (
     <div>
@@ -39,13 +49,16 @@ const App = () => {
           name: <input value = {newName} onChange = {handleNameChange}/>
         </div>
         <div>
+          number: <input value = {newNumber} onChange = {handleNumberChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map(person => {
-         return <People person = {person.name} key = {person.id} /> 
+         return <People person = {person.name} key = {person.id} number = {person.number} /> 
         })}
         
       </ul>
--- part2/phonebook/src/components/People.js
+++ part2/phonebook/src/components/People.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const People = ({person}) =>{
+const People = ({person,number}) =>{
     return(
-        <li>{person}</li>
+        <li>{person} {number}</li>
     )
     
 }
