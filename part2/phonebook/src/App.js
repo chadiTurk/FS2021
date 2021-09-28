216a1fa15f58ac446f66ebb6e569e4186452fece
Chadi <chadi_turk@dlsu.edu.ph>
2021-09-28 15:34:44 +0800

    phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,10 +5,20 @@ import People from './components/People'
 
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
+  const[ filterName, setFilterName] = useState('')
+
+  const [ filterPersons,setFilterPersons] = useState(persons)
+
+
 
   const addPerson = (event) =>{
     event.preventDefault()
@@ -38,12 +48,35 @@ const App = () => {
   const handleNumberChange = (event) =>{
     setNewNumber(event.target.value)
   }
+
+  const handleNameFilter = (event) =>{
+    setFilterName(event.target.value)
+
+    if(isEmptyOrSpaces(event.target.value)){
+      return
+    }
+
+    console.log(event.target.value)
     
+    const tempFilterPersons = persons.filter(person =>
+      (person.name.toLowerCase()).includes((event.target.value).toLowerCase())
+    )
+    setFilterPersons(tempFilterPersons)
+  }
+  
+  function isEmptyOrSpaces(str){
+    return str === null || str.match(/^ *$/) !== null;
+}
   
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with<input value = {filterName} onChange = {handleNameFilter}/>
+      </div>
+      
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value = {newName} onChange = {handleNameChange}/>
@@ -57,10 +90,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => {
+        {filterPersons.map(person => {
          return <People person = {person.name} key = {person.id} number = {person.number} /> 
         })}
-        
       </ul>
     </div>
   )
