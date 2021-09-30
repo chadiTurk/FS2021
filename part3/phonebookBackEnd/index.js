b639dca79843d78a4f595e812eebe1d98f46ee89
Chadi <chadi_turk@dlsu.edu.ph>
2021-09-30 18:18:07 +0800

    phonebook backend step2

--- part3/phonebookBackEnd/index.js
+++ part3/phonebookBackEnd/index.js
@@ -28,6 +28,16 @@ app.get('/api/persons', (request,response) =>{
     response.json(persons)
 })
 
+app.get('/info',(request,response)=>{
+    const numberOfPeople = persons.length
+
+    response.send(`
+    <p> Phonebook has info for ${numberOfPeople} people </p>
+    <p> ${new Date} </p>
+    `)
+    response
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
