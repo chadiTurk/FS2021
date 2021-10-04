8f8e8561bbe1b51f48b012aacb2ba380ea90b820
Chadi <chadi_turk@dlsu.edu.ph>
2021-10-04 13:56:11 +0800

    helper functions and unit tests step2 (all tests passed)

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,16 @@
-const dummy = (blogs) =>{
+const dummy = blogs =>{
     return 1
 }
 
+const totalLikes = blogs =>{
+    let total = 0
+
+    blogs.map(blog => total += blog.likes)
+
+    return total
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
