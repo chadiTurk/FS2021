562e6b8d6f26258dd8c3673585202289fac5fa7e
Chadi <chadi_turk@dlsu.edu.ph>
2021-10-06 12:51:06 +0800

    Blog lists tests step 3

--- part4/tests/http.test.js
+++ part4/tests/http.test.js
@@ -2,7 +2,7 @@ const supertest = require('supertest')
 const mongoose = require('mongoose')
 const app = require('../app')
 const api = supertest(app)
-
+const helper = require('./http_helper')
 describe('HTTP GET',()=>{
 
     test('total amount of blogs is correct', async ()=>{
@@ -10,7 +10,7 @@ describe('HTTP GET',()=>{
         .get('/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
-        expect(response.body.length).toBe(1)
+        expect(response.body.length).toBe(5)
     })
 
     test('_id is replaced as id in the Blog model', async ()=>{
@@ -20,7 +20,25 @@ describe('HTTP GET',()=>{
         .expect('Content-Type', /application\/json/)
         response.body.map(blog => expect(blog.id).toBeDefined())
     })
+})
 
+describe('HTTP PUT',()=>{
+
+    test('a new blog can be added to the database',async()=>{    
+     const initialResponse = await api.get('/blogs')
+
+        await api
+        .post('/blogs')
+        .send(helper.sampleNote)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+        const secondResponse = await api.get('/blogs')
+
+        expect(secondResponse.body.length).toBe(initialResponse.body.length + 1)
+        
+
+    })
 })
 
 afterAll(() => {
---
+++ part4/tests/http_helper.js
@@ -0,0 +1,11 @@
+
+const sampleNote = {
+    title:'Monroe',
+    author:'Seecret',
+    url:'hellothere',
+    likes:0
+}
+
+module.exports = {
+    sampleNote
+}
