5e9c814d94a8cbf8c47eee0960375bc5d2173abe
Chadi <chadi_turk@dlsu.edu.ph>
2021-10-10 11:26:24 +0800

    Blog list front end steps 6 & 7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,33 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React,{useState} from 'react'
+const Blog = (props) => {
+
+  const [visibility,setVisibility] = useState(true)
+  const [buttonLabel,setButtonLabel] = useState('show')
+  const hideWhenVisible = {display : visibility ? 'none' : ''}
+
+  const toggleVisibility = () =>{
+    setVisibility(!visibility)
+    if(buttonLabel === 'none'){
+      setButtonLabel('show')
+    }
+    else{
+      setButtonLabel('none')
+    }
+  }
+  
+
+  return(
+    <div>
+      <span> Title: {props.blog.title}</span><button onClick={toggleVisibility}>{buttonLabel}</button>
+      <p style = {hideWhenVisible}>Url:{props.blog.url}</p>
+      <p style = {hideWhenVisible}>Likes:{props.blog.likes}</p>
+    </div>
+  )
+}
+
+
+
+ 
+
 
 export default Blog
